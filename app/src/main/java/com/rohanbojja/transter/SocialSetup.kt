f774d91d715e3262acae240677b120fcb6b54fa4
Rohan Bojja <rohanbojja@outlook.com>
2020-03-23 06:17:18 +0530

    - Added placeholder activities.

---
+++ app/src/main/java/com/rohanbojja/transter/SocialSetup.kt
@@ -0,0 +1,11 @@
+package com.rohanbojja.transter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SocialSetup : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
